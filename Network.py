72677e323d6f7ef4cef351cf5b81c2c7fb28d5d9
ShepperA <62827752+ShepperA@users.noreply.github.com>
2023-06-18 16:25:06 +0300

    Add files via upload

---
+++ Network.py
@@ -0,0 +1,77 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+
+train_dir = 'train'
+
+val_dir = 'val'
+
+test_dir = 'test'
+
+img_width, img_height = 750, 500
+input_shape = (img_width, img_height, 3)
+
+epochs = 2
+batch_size = 2
+
+nb_train_samples = 160
+nb_validation_samples = 20
+nb_test_samples = 20
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+datagen = ImageDataGenerator(rescale=1. / 255)
+train = datagen.flow_from_directory(
+    train_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary',
+    classes=['safe', 'critical'])
+
+val = datagen.flow_from_directory(
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary',
+    classes=['safe', 'critical'])
+
+test = datagen.flow_from_directory(
+    test_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary',
+    classes=['safe', 'critical'])
+
+model.fit(
+    train,
+    steps_per_epoch=nb_train_samples,
+    epochs=epochs,
+    batch_size=batch_size,
+    validation_data=val,
+    validation_steps=nb_validation_samples)
+
+scores = model.evaluate_generator(test, nb_test_samples // batch_size)
+print("Аккуратность на тестовых данных: %.2f%%" % (scores[1]*100))
+
+model.save('binary.h5')
---
+++ interface.py
@@ -0,0 +1,120 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+import keras
+import keras.utils as image
+from PyQt5.QtCore import Qt
+import numpy as np
+
+model = keras.models.load_model('binary.h5')
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setMaximumSize(QtCore.QSize(800, 600))
+        MainWindow.setStyleSheet("background-color: rgb(239, 254, 255);\n"
+                                 "")
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(-20, 0, 820, 60))
+        self.label.setStyleSheet("background-color: rgb(94, 94, 94);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+
+        self.photo_label = QtWidgets.QLabel(self.centralwidget)
+        self.photo_label.setGeometry(QtCore.QRect(120, 85, 570, 420))
+        self.photo_label.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.photo_label.setObjectName("photo_label")
+
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(0, 495, 780, 30))
+        self.label_3.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.label_3.setObjectName("label_3")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        font = QtGui.QFont()
+        font.setFamily("Franklin Gothic Heavy")
+        font.setPointSize(15)
+        self.label_3.setFont(font)
+
+        self.label_persnt = QtWidgets.QLabel(self.centralwidget)
+        self.label_persnt.setGeometry(QtCore.QRect(720, 495, 60, 30))
+        self.label_persnt.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.label_persnt.setObjectName("label_persnt")
+        self.label_persnt.setAlignment(Qt.AlignCenter)
+
+        fontper = QtGui.QFont()
+        fontper.setFamily("Franklin Gothic Heavy")
+        fontper.setPointSize(13)
+        self.label_persnt.setFont(fontper)
+
+        self.ZagrFoto = QtWidgets.QPushButton(self.centralwidget)
+        self.ZagrFoto.setGeometry(QtCore.QRect(0, 540, 800, 60))
+        self.ZagrFoto.setMaximumSize(QtCore.QSize(16777215, 16777215))
+
+        self.ZagrFoto.setFont(font)
+        self.ZagrFoto.setStyleSheet(
+            "background-color: rgb(19, 22, 43);\n"
+            "background-color: rgb(94, 94, 94);\n"
+            "color: rgb(255, 255, 255);\n"
+            "")
+        self.ZagrFoto.setObjectName("ZagrFoto")
+        self.ZagrFoto.clicked.connect(self.open_file_dialog)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Оценка участка"))
+        self.label_3.setText(_translate("MainWindow", ""))
+        self.label_persnt.setText(_translate("MainWindow", ""))
+        self.ZagrFoto.setText(_translate("MainWindow", "Загрузка и Анализ изображения"))
+
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        file_name, _ = QFileDialog.getOpenFileName(None, "Выберите изображение", "",
+                                                   "Images (*.png *.jpg *.bmp);;All Files (*)",
+                                                   options=options)
+
+        img = image.load_img(file_name, target_size=(750, 500))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = x / 255.0
+
+        if file_name:
+            self.load_image(file_name)
+
+        predictions = model.predict(x)
+        prediction_description = ' '.join([str(round(p * 100)) + '%' for p in predictions[0]])
+        print(predictions[0][0])
+        self.label_persnt.setText(prediction_description)
+
+        if predictions[0][0] > 0.5:
+            self.label_3.setText('Участок повышенной пожароопасности')
+        else:
+            self.label_3.setText('Безопасный участок')
+
+    def load_image(self, file_path):
+        pixmap = QtGui.QPixmap(file_path)
+        pixmap = pixmap.scaled(self.photo_label.width(), self.photo_label.height(),
+                               QtCore.Qt.KeepAspectRatio)
+        self.photo_label.setPixmap(pixmap)
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
